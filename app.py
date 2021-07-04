47c12c5b70ebbed67ccf012300bca0c8be0c2789
saikumar <80379608+saikumar725@users.noreply.github.com>
2021-07-04 11:06:43 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,25 +1,31 @@
-# Importing essential libraries
-from flask import Flask, render_template, request
-import pickle
-
-# Load the Multinomial Naive Bayes model and CountVectorizer object from disk
-filename = 'spam-sms-mnb-model.pkl'
-classifier = pickle.load(open(filename, 'rb'))
-cv = pickle.load(open('cv-transform.pkl','rb'))
+from flask import Flask,render_template,redirect,session,url_for,request
+from flask_mysqldb import MySQL
+import MySQLdb
 app = Flask(__name__)
+app.secret_key='saikumar'
 
-@app.route('/')
-def home():
-	return render_template('home.html')
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = " Admin@1234"
+app.config["MYSQL_DB"] = "login"
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        message = request.form['message']
-        data = [message]
-        vect = cv.transform(data).toarray()
-        my_prediction = classifier.predict(vect)
-        return render_template('home.html', prediction=my_prediction)
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    if request.method=='POST':
+        if 'username' in request.form and 'password' in request.form:
+            username = request.form['username']
+            password = request.form['password']
+            cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute("SELECT * FROM logininfo WHERE username=%s AND password=%s",(username,password))
+            info = cursor.fetchone()
+            if info['username'] == username and info['password'] == password:
+                return "login successfull"
+            else:
+                return "login unsuccessful,please register"
+    return render_template("login.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
